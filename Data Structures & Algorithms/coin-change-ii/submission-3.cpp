a5be27ca4fd1a0caaafe35d134bc5311ca3fb167
vOid9201 <100140465+vOid9201@users.noreply.github.com>
2026-06-19 09:07:30 +0530

    Add: coin-change-ii - submission-3

---
+++ Data Structures & Algorithms/coin-change-ii/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int solve(int i,int amount,vector<int>&coins,vector<vector<int>>&dp){
+	if(i < 0 and amount > 0) return 0;
+	if(amount == 0) return 1;
+	if(dp[i][amount] != -1) return dp[i][amount];
+	
+	if(coins[i] <= amount)
+		dp[i][amount] = solve(i-1,amount,coins,dp)+
+		solve(i,amount-coins[i],coins,dp);
+	else 
+		dp[i][amount] = solve(i-1,amount,coins,dp);
+	return dp[i][amount];
+}
+
+int change(int amount,vector<int> &coins){
+	int n = coins.size();
+	vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+	return solve(n-1,amount,coins,dp);
+}
+};
