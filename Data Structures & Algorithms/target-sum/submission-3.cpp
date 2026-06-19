c7e1579f8aa73303256366fd1906f594043f3df7
vOid9201 <100140465+vOid9201@users.noreply.github.com>
2026-06-19 10:42:36 +0530

    Add: target-sum - submission-3

---
+++ Data Structures & Algorithms/target-sum/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+int findTargetSumWays(vector<int>&nums,int target){
+	int total_sum = 0ll , n = nums.size();
+	for(auto x : nums) total_sum += x;
+	vector<vector<int>> dp(n+1,vector<int>(2*total_sum+1,0));
+	dp[0][total_sum] = 1;
+	for(int i=1;i<n+1;i++){
+		for(int j=0;j<2*total_sum+1;j++){
+			if(dp[i-1][j] == 0) continue;
+			else{
+				cout << i << " " << j << " " << j+nums[i-1] << '\n';
+				cout << i << " " << j << " " << j-nums[i-1] << '\n';
+
+				dp[i][j+nums[i-1]] += dp[i-1][j];
+				dp[i][j-nums[i-1]] += dp[i-1][j];
+
+				// cout << 
+			}
+		}
+	}
+	
+	if(target+total_sum >=0 and target+total_sum < 2*total_sum+1)
+		return dp[n][target+total_sum];
+	return 0;
+}
+};
